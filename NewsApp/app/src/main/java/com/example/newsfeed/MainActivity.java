c9d92f90742d3ef065206ed334505e1cbb18b4e2
Hakimi Ridzuan <mrhakimi88@gmail.com>
2017-08-13 14:41:54 +0800

    Add files via upload

---
+++ NewsApp/app/src/main/java/com/example/newsfeed/MainActivity.java
@@ -0,0 +1,90 @@
+package com.example.newsfeed;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MainActivity
+        extends AppCompatActivity
+        implements LoaderManager.LoaderCallbacks<List<News>> {
+
+    private static int LOADER_ID = 0;
+
+    private NewsAdapter adapter;
+
+    private TextView mEmptyStateTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView) findViewById(R.id.list_view);
+
+        adapter = new NewsAdapter(this);
+        listView.setAdapter(adapter);
+
+        mEmptyStateTextView = (TextView) findViewById(R.id.empty_view);
+        listView.setEmptyView(mEmptyStateTextView);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                News news = adapter.getItem(i);
+                String url = news.url;
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        if (networkInfo != null && networkInfo.isConnected()) {
+            LoaderManager loaderManager = getSupportLoaderManager();
+            loaderManager.initLoader(LOADER_ID, null, this);
+
+        } else {
+            mEmptyStateTextView.setText(R.string.no_internet_connection);
+        }
+    }
+
+    @Override
+    public Loader<List<News>> onCreateLoader(int id, Bundle args) {
+        return new NewsLoader(this);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<News>> loader, List<News> data) {
+
+        mEmptyStateTextView.setText(R.string.no_data);
+        adapter.clear();
+
+        if (data != null) {
+            adapter.setNotifyOnChange(false);
+            adapter.clear();
+            adapter.setNotifyOnChange(true);
+            adapter.addAll(data);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<News>> loader) {
+    }
+
+}
